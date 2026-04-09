2e9cb5032e237d3c2b7c409643ce191f2425c09d
codecroo <parthdelvadiya1475@gmail.com>
2026-04-09 12:18:36 +0530

    SOD

---
+++ PY File IO/CONFUSING.py
@@ -0,0 +1,13 @@
+f = open("file.txt", "r+")
+# f.seek(offset, position)
+
+# 🔍 Parameters:
+# offset → number of bytes to move
+# position (whence) → from where to move:
+
+
+# | Value | Meaning                          |
+# | ----- | -------------------------------- |
+# |   0   | From **start** of file (default) |
+# |   1   | From **current position**        |
+# |   2   | From **end of file**             |
